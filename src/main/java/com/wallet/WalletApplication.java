0e52f10d7e5f989f3df4fe01dc2c0afdd632db97
Vitor <vitor@MacBook-Pro-de-Vitor.local>
2019-08-19 21:46:27 -0300

    Commit inicial

---
+++ src/main/java/com/wallet/WalletApplication.java
@@ -0,0 +1,13 @@
+package com.wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletApplication.class, args);
+	}
+
+}
