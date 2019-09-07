19197a234b7f9ae556cdbbcb0ef7265c2459f95b
Vitor <vitor.diogo.live@hotmail.com>
2019-09-07 14:43:40 -0300

    voltando UserWalletRepository

--- src/main/java/com/wallet/repository/UserWalletRepository.java
+++ src/main/java/com/wallet/repository/UserWalletRepository.java
@@ -2,8 +2,8 @@ package com.wallet.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-//import com.wallet.entity.UserWallet;
+import com.wallet.entity.UserWallet;
 
-//public interface UserWalletRepository  extends JpaRepository<UserWallet, Long>{
+public interface UserWalletRepository  extends JpaRepository<UserWallet, Long>{
 
-//}
+}
