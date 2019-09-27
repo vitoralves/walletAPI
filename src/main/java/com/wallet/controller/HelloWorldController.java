89719bfc2b3c79ff06b44131e967fecc7e627b10
Vitor <vitor.diogo.live@hotmail.com>
2019-09-26 21:29:01 -0300

    get mapping

--- src/main/java/com/wallet/controller/HelloWorldController.java
+++ src/main/java/com/wallet/controller/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.wallet.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import com.wallet.response.Response;
 @RequestMapping("hello-world")
 public class HelloWorldController {
 	
+	@GetMapping
 	public ResponseEntity<Response<String>> get() {
 		Response<String> response = new Response<String>();
 		response.setData("Hello World!!!!!!  ");
